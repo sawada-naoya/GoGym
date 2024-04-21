97ed73abadfb3171f61de4cf4deb83e697b60831
applepunch5 <naoya.sawa.824@gmail.com>
2024-04-21 09:48:36 +0900

    ログインページのレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'top#index'
 
+  get '/login', to: 'user_sessions#new', as: :login
+  post '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy', as: :logout
+
   resources :top, only: %i[index]
   resources :users, only: %i[new create]
+
 
   resources :gyms do
     collection do
