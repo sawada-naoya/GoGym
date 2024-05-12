21389203ca848aa043cd1b5709d19fc0a58f0d7d
applepunch5 <naoya.sawa.824@gmail.com>
2024-05-12 14:36:46 +0900

    ジム登録機能の作成

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,4 +1,24 @@
 class GymsController < ApplicationController
+  # ユーザーがログインしているかどうかをチェック
+  before_action :require_login, only: [:new, :create]
+
+  # GET /gyms/new
+  def new
+    @gym = Gym.new
+    @gym.build_location
+  end
+
+  # POST /gyms
+  def create
+    @gym = Gym.new(gym_params)
+    if @gym.save
+      redirect_to @gym
+      flash[:success] = t('flash.gym_create_success')
+    else
+      render :new, status: :unprocessable_entity
+      flash.now[:danger] = t('flash.gym_create_failure')
+    end
+  end
 
   def index
     @gyms = @q.result(distinct: true).page(params[:page]).per(5)
@@ -16,5 +36,12 @@ class GymsController < ApplicationController
 
   # データの編集画面を表示
   def edit; end
+
+  private
+
+  def gym_params
+    params.require(:gym).permit(:name, :membership_fee, :business_hours, :access, :remarks, :website,
+    location_attributes: [:address])
+  end
 
 end
