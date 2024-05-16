9c8d5a619259e57af1b27a4c94f85b11d5d07774
applepunch5 <naoya.sawa.824@gmail.com>
2024-05-16 23:00:04 +0900

    利用規約・プライバシーポリシーポリシーの作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,6 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/login', to: 'user_sessions#new', as: :login
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy', as: :logout
+  get '/terms', to: 'static_pages#terms'
+  get '/privacy', to: 'static_pages#privacy'
 
   resources :top, only: %i[index]
   resources :inquiries, only: [:new, :create]
