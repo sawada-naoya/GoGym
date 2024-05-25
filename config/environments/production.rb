0d3c49b9da96991fe5bf7708b8f2065f0caf2f7e
applepunch5 <naoya.sawa.824@gmail.com>
2024-05-25 16:59:41 +0900

    edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,11 +11,13 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   config.assets.compile = false
+  config.assets.digest = true
+  config.assets.prefix = "/assets"
 
   config.active_storage.service = :local
 
   config.log_level = :info
-  
+
   config.log_tags = [ :request_id ]
 
   config.action_mailer.perform_caching = false
