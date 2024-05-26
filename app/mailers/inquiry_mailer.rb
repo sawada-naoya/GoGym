ee7f41fec0c1958d8948a4e760f6bf2591e33dda
applepunch5 <naoya.sawa.824@gmail.com>
2024-05-26 21:27:48 +0900

    a

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,10 +1,10 @@
 class InquiryMailer < ApplicationMailer
-  default from: "from@example.com"
+  # default from: "zetianzhimi7@gmail.com"
   layout "mailer"
 
   def new_inquiry(inquiry)
     @inquiry = inquiry
-    mail(from: @inquiry.email, to: 'example@gmail.com', subject: 'Webサイトより問い合わせが届きました') do |format|
+    mail(from: @inquiry.email, to: 'zetianzhimi7@gmail.com', subject: 'Webサイトより問い合わせが届きました') do |format|
       format.text
     end
   end
