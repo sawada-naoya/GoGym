a80eeb54324cfce3ab2974bc31eda5e1199a0dac
applepunch5 <naoya.sawa.824@gmail.com>
2024-06-24 23:24:12 +0900

    レスポンシブデザイン修正７割完了

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,15 +4,5 @@
 //= require bootstrap-sprockets
 //= require raty
 //= require loading
+//= require custom
 //= require_tree .
-
-// ページ内のすべてのクラスが btn-outline-success である要素を選択
-document.addEventListener("DOMContentLoaded", function() {
-  const buttons = document.querySelectorAll(".btn-outline-success");
-  buttons.forEach(function(button) {
-    if (button.getAttribute("data-url") === window.location.pathname) {
-      button.classList.add("btn-success");
-      button.classList.remove("btn-outline-success");
-    }
-  });
-});
