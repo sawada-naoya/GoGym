95789a25c1240c73e5fb02a6e4bd2a5cc4ea333a
applepunch5 <naoya.sawa.824@gmail.com>
2024-08-12 17:22:55 +0900

    修正

--- config/environments/debug_env.rb
+++ config/environments/debug_env.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Rails.logger.info "AWS_ACCESS_KEY_ID: #{ENV['AWS_ACCESS_KEY_ID']}"
-  Rails.logger.info "AWS_SECRET_ACCESS_KEY: #{ENV['AWS_SECRET_ACCESS_KEY']}"
-  Rails.logger.info "AWS_BUCKET_NAME: #{ENV['AWS_BUCKET_NAME']}"
+  puts "AWS_ACCESS_KEY_ID: #{ENV['AWS_ACCESS_KEY_ID']}"
+  puts "AWS_SECRET_ACCESS_KEY: #{ENV['AWS_SECRET_ACCESS_KEY']}"
+  puts "AWS_BUCKET_NAME: #{ENV['AWS_BUCKET_NAME']}"
 end
