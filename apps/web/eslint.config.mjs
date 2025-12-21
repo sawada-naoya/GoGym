b50ba56d3d29bad19b9307370047154162f3de26
sawada-naoya <naoya.sawa.bb@gmail.com>
2025-12-21 23:03:43 +0900

    wip

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,11 +1,23 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from "@eslint/compat";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import globals from "globals";
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
-
-export default eslintConfig;
